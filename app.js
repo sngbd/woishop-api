2c5270dbb1941492da4ec21847884bc93162ee24
Gilberdi Sinaga <gilberdising012@gmail.com>
2022-08-23 15:31:17 +0700

    init study case 2

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const productsRouter = require('./controllers/products');
+const usersRouter = require('./controllers/products');
 
 app.use(cors());
 
 app.use('/api/products', productsRouter);
+app.use('/api/users', usersRouter);
 
 module.exports = app;
