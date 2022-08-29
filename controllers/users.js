932958289458792c8d7b0cc7606b09b4865604f2
Gilberdi Sinaga <gilberdising012@gmail.com>
2022-08-29 13:04:25 +0700

    implement email verification

--- controllers/users.js
+++ controllers/users.js
@@ -1,7 +1,49 @@
 const bcrypt = require('bcrypt');
 const usersRouter = require('express').Router();
+const nodemailer = require('nodemailer');
 const pool = require('../data/db');
 const response = require('../helpers/response');
+
+const { AUTH_EMAIL, AUTH_PASS } = process.env;
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  auth: {
+    user: AUTH_EMAIL,
+    pass: AUTH_PASS,
+  },
+});
+
+const sendOTP = async ({ id, email }, res) => {
+  const otp = `${Math.floor(1000 + Math.random() * 9000)}`;
+
+  const mailOptions = {
+    from: AUTH_EMAIL,
+    to: email,
+    subject: 'Verify your email',
+    html: `<p>Enter <b>${otp}</b> in the app 
+    to verify your email address and complete the registration.</p>
+    <p>This code expires in 1 hour.</p>`,
+  };
+
+  const saltRounds = 10;
+  const hashedOTP = await bcrypt.hash(otp, saltRounds);
+
+  await pool.query(
+    'INSERT INTO otp VALUES (DEFAULT, $1, $2, $3, $4)',
+    [id, Date.now(), Date.now() + 3600000, hashedOTP],
+  );
+
+  await transporter.sendMail(mailOptions);
+
+  return res.json(
+    response(true, 'Verification OTP email sent', {
+      status: 'Pending',
+      user_id: id,
+      email,
+    }),
+  );
+};
 
 usersRouter.get('/', async (_req, res) => {
   const { rowCount, rows } = await pool.query('SELECT * FROM users');
@@ -26,13 +68,14 @@ usersRouter.post('/', async (req, res) => {
   const {
     username,
     name,
+    email,
     phone_number,
     password,
   } = req.body;
 
-  if (!username || !password) {
+  if (!username || !password || !email || !phone_number || !name) {
     return res.status(404).json(
-      response(false, 'Username or password is missing', {}),
+      response(false, 'Empty users details are not allowed', {}),
     );
   }
 
@@ -59,13 +102,72 @@ usersRouter.post('/', async (req, res) => {
   const passwordHash = await bcrypt.hash(password, saltRounds);
 
   const { rows } = await pool.query(
-    'INSERT INTO users VALUES (DEFAULT, $1, $2, $3, $4) RETURNING username, name, phone_number',
-    [username, name, phone_number, passwordHash],
+    'INSERT INTO users VALUES (DEFAULT, $1, $2, $3, $4, $5, false) RETURNING id, email',
+    [username, name, email, phone_number, passwordHash],
   );
 
-  return res.status(201).json(
-    response(true, 'User is saved', rows),
+  return sendOTP(rows[0], res);
+});
+
+usersRouter.post('/verify', async (req, res) => {
+  const { user_id, otp } = req.body;
+  if (!user_id || !otp) {
+    return res.status(404).json(
+      response(false, 'Empty OTP details are not allowed', {}),
+    );
+  }
+
+  const { rowCount, rows } = await pool.query('SELECT * FROM otp WHERE user_id=$1', [user_id]);
+
+  if (!rowCount) {
+    return res.status(404).json(
+      response(
+        false,
+        'Account doesn\'t exist or has been verified already. Please register or login.',
+        {},
+      ),
+    );
+  }
+
+  const { expires_at } = rows[0];
+  const hashedOTP = rows[0].otp;
+
+  if (expires_at < Date.now()) {
+    await pool.query('DELETE FROM otp WHERE user_id=$1', [user_id]);
+    return res.status(404).json(
+      response(false, 'Code has expired. Please request again.', {}),
+    );
+  }
+
+  const validOTP = await bcrypt.compare(otp, hashedOTP);
+
+  if (!validOTP) {
+    return res.status(404).json(
+      response(false, 'Invalid code passed. Please check your inbox.', {}),
+    );
+  }
+
+  await pool.query('UPDATE users SET verified=true WHERE id=$1', [user_id]);
+  await pool.query('DELETE FROM otp WHERE user_id=$1', [user_id]);
+
+  return res.json(
+    response(true, 'users email verified successfully.', {
+      status: 'Verified',
+      user_id,
+    }),
   );
+});
+
+usersRouter.post('/resendOTP', async (req, res) => {
+  const { user_id, email } = req.body;
+
+  if (!user_id || !email) {
+    return res.status(404).json(
+      response(false, 'Empty users details are not allowed', {}),
+    );
+  }
+  await pool.query('DELETE FROM otp WHERE user_id=$1', [user_id]);
+  return sendOTP({ id: user_id, email }, res);
 });
 
 module.exports = usersRouter;
--- data/users.sql
+++ data/users.sql
@@ -4,6 +4,17 @@ CREATE TABLE IF NOT EXISTS users(
   id SERIAL PRIMARY KEY,
   username VARCHAR(25) NOT NULL,
   name VARCHAR(50) NOT NULL,
+  email TEXT NOT NULL,
   phone_number TEXT NOT NULL,
-  password_hash TEXT NOT NULL
+  password_hash TEXT NOT NULL,
+  verified BOOLEAN NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS otp(
+  id SERIAL PRIMARY KEY,
+  user_id INTEGER NOT NULL,
+  created_at BIGINT NOT NULL,
+  expires_At BIGINT NOT NULL,
+  otp TEXT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE SET NULL
+)
