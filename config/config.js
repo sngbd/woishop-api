f7f5865176e2ed41e549ced04628e41f535f4d5e
Gilberdi Sinaga <gilberdising012@gmail.com>
2022-09-18 18:53:20 +0700

    refact: use environment variables in sequelize config

--- config/config.js
+++ config/config.js
@@ -4,6 +4,8 @@ const {
   DB_USERNAME,
   DB_PASSWORD,
   DB_DEV,
+  DB_TEST,
+  DB_PROD,
   DB_HOST,
 } = process.env;
 
@@ -19,21 +21,20 @@ module.exports = {
     },
   },
   test: {
-    username: 'me',
-    password: 'passwd',
-    database: 'woishop_test',
-    host: '127.0.0.1',
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_TEST,
+    host: DB_HOST,
     dialect: 'postgres',
     define: {
       timestamps: false,
     },
   },
   production: {
-    username: 'me',
-    password: 'passwd',
-    database: 'woishop_prod',
-    host: '127.0.0.1',
-    dialect: 'postgres',
+    username: DB_USERNAME,
+    password: DB_PASSWORD,
+    database: DB_PROD,
+    host: DB_HOST,
     define: {
       timestamps: false,
     },
